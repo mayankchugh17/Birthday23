a9f4216c4b6626475328863202bb25245e69672f
mayankchugh17 <mayankchugh222@gmail.com>
2026-01-31 23:03:52 +0530

    Special Page updated

--- src/app/utils/cardsData.js
+++ src/app/utils/cardsData.js
@@ -1,12 +1,12 @@
 const cardsData = [
-  {flower:'/cards/lavender.jpg', title: 'Lavender', description: "You are lavender in human form: gentle, graceful, and unforgettable."},
-  {flower:'/cards/carnation1.jpg', title: 'Carnation', description: 'You are carnation hearted: graceful, patient, and beautifully loyal.'},
-  {flower:'/cards/ose.jpg', title: 'Rose', description: 'You are rose: soft in beauty, strong in soul, and deeply loved in every season.'},
-  {flower:'/cards/sunflower.jpg', title: 'Sunflower', description: 'You are sunflower: bright, bold, and always turning toward the light.'},
-  {flower:'/cards/tulips.jpg', title: 'Tulip', description: 'You are tulip: elegant, vibrant, and a symbol of perfect love.'},
-  {flower:'/cards/lily.jpg', title: 'Lily', description: 'You are lily: pure, graceful, and full of gentle beauty.'},
-  {flower:'/cards/peony.jpg', title: 'Peony', description: 'You are peony-graceful, radiant, and a love that blooms fuller with every moment.'},
-  {flower:'/cards/orchid.jpg', title: 'Orchid', description: 'You are an orchid—rare, elegant, and beautifully unforgettable.'},
+  {flower:'/cards/lavender.jpg', title: 'Lavender', description: "You're lavender in human form: gentle, graceful, and unforgettable."},
+  {flower:'/cards/carnation1.jpg', title: 'Carnation', description: "You're carnation hearted: graceful, patient, and beautifully loyal."},
+  {flower:'/cards/ose.jpg', title: 'Rose', description: "You're rose: soft in beauty, strong in soul, and deeply loved in every season."},
+  {flower:'/cards/sunflower.jpg', title: 'Sunflower', description: "You're sunflower: bright, bold, and always turning toward the light."},
+  {flower:'/cards/tulips.jpg', title: 'Tulip', description: "You're tulip: elegant, vibrant, and a symbol of perfect love."},
+  {flower:'/cards/lily.jpg', title: 'Lily', description: "You're lily: pure, graceful, and full of gentle beauty."},
+  {flower:'/cards/peony.jpg', title: 'Peony', description: "You're peony-graceful, radiant, and a love that blooms fuller with every moment."},
+  {flower:'/cards/orchid.jpg', title: 'Orchid', description: "You're an orchid—rare, elegant, and beautifully unforgettable."},
 ];
 
 export default cardsData;
